1d6ca078bcd4c20f0ed552ff040747f727075b33
Chris Kemp <ckemp@salesforce.com>
2012-11-12 18:43:12 -0500

    Adding basic authentication

--- web.rb
+++ web.rb
@@ -1,6 +1,20 @@
-# web.rb:
+# web.rb
 require 'sinatra'
-get '/' do
-     "Hello, " << ENV['NAME']
+helpers do
+        def protected!
+        unless ENV['RACK_ENV'] != 'staging' || authorized?
+                response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+                throw(:halt, [401, "Not authorized\n"])
+        end
+        end
+        def authorized?
+        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+        @auth.provided? && @auth.basic? && @auth.credentials && 
+          @auth.credentials == ['alladin', 'opensesame']
+        end
 end
 
+get '/' do
+        protected!
+        "Hello, " << ENV['NAME']
+end
